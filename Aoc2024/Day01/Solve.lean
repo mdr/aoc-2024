41312f51cb393925650bc2074e7859ab7cfa18dd
Matt Russell <MattRussellUK@gmail.com>
2024-12-01 17:10:59 +0000

    wip

--- Aoc2024/Day01/Solve.lean
+++ Aoc2024/Day01/Solve.lean
@@ -8,15 +8,15 @@ private def solvePart1 (pairs : List (Int × Int)) : Int :=
   let distance (a b : Int) : Int := (a - b).natAbs
   (firsts.mergeSort.zipWith distance seconds.mergeSort).sum
 
-def parseAndSolvePart1 (s : String): Except String Int := parseLines s |> .map solvePart1
+def parseAndSolvePart1 (s : String): Except String Int := parseLines s |>.map solvePart1
 
-#guard (parseAndSolvePart1 exampleInput == Except.ok 11)
+#guard parseAndSolvePart1 exampleInput == Except.ok 11
 
 private def solvePart2 (pairs : List (Int × Int)) : Int :=
   let (firsts, seconds) := pairs.unzip
   let similarityScore (n : Int) : Int := seconds.count n * n
   firsts.sumBy similarityScore
 
-def parseAndSolvePart2 (s : String): Except String Int := parseLines s |> .map solvePart2
+def parseAndSolvePart2 (s : String): Except String Int := parseLines s |>.map solvePart2
 
-#guard (parseAndSolvePart2 exampleInput == Except.ok 31)
+#guard parseAndSolvePart2 exampleInput == Except.ok 31
