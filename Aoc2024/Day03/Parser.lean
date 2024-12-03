412a9acdad6dec72595e00fa2b59a84cdafc6875
Matt Russell <MattRussellUK@gmail.com>
2024-12-03 18:38:43 +0000

    wip

--- Aoc2024/Day03/Parser.lean
+++ Aoc2024/Day03/Parser.lean
@@ -4,14 +4,14 @@ import Std
 import Regex
 open Regex (Match Captures)
 
-def instructionRegex: Regex := regex% r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)"
+private def instructionRegex: Regex := regex% r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)"
 
 private def matchToMultiplyInstruction (captures: Captures): Option Instruction :=
   match captures.groups.toList with
   | [some m1, some m2] => Instruction.mul <$> m1.toNat? <*> m2.toNat?
   | _ => none
 
-def matchToInstruction (captures: Captures) : Option Instruction :=
+private def matchToInstruction (captures: Captures) : Option Instruction :=
   if captures.fullMatch == "do()" then some Instruction.do
   else if captures.fullMatch == "don't()" then some Instruction.dont
   else matchToMultiplyInstruction captures
