9973b1b53d43501e6735d5c766d966abd8d5965d
Matt Russell <MattRussellUK@gmail.com>
2024-12-03 18:37:22 +0000

    State monad

--- Aoc2024/Day03/Solve.lean
+++ Aoc2024/Day03/Solve.lean
@@ -3,7 +3,7 @@ import Aoc2024.Day03.Examples
 import Aoc2024.Day03.Parser
 import Aoc2024.Day03.Types
 
-def Instruction.value : Instruction -> Int
+private def Instruction.value : Instruction -> Int
   | Instruction.mul n m => n * m
   | _ => 1
 
@@ -11,18 +11,27 @@ def solvePart1 (s : String) : Int := findInstructions s |>.sumBy (·.value)
 
 #guard solvePart1 exampleInputPart1 == 161
 
-private def findEnabledMulInstructions (enabled : Bool) : List Instruction -> List Instruction
-  | [] => []
-  | instruction :: rest =>
-    match instruction with
-      | Instruction.mul m n =>
-        if enabled then
-          Instruction.mul m n :: findEnabledMulInstructions enabled rest
-        else
-          findEnabledMulInstructions enabled rest
-      | Instruction.do => findEnabledMulInstructions true rest
-      | Instruction.dont => findEnabledMulInstructions false rest
+private structure State where
+  isEnabled: Bool
+  enabledMuls: List Instruction
 
-def solvePart2 (s : String) : Int := findInstructions s |> findEnabledMulInstructions true |>.sumBy (·.value)
+def State.enable (s : State): State := { s with isEnabled := true }
+def State.disable (s : State): State := { s with isEnabled := false }
+def State.recordMul (instruction : Instruction) (s : State): State :=
+  { s with enabledMuls := instruction :: s.enabledMuls }
+def State.initial: State := { isEnabled := true, enabledMuls := [] }
+
+private def processInstruction (instruction: Instruction): StateM State Unit :=
+  match instruction with
+    | Instruction.mul _ _ => do
+        if (<- get).isEnabled then
+          modify (State.recordMul instruction)
+    | Instruction.do => modify State.enable
+    | Instruction.dont => modify State.disable
+
+private def getEnabledMuls (instructions : List Instruction) : List Instruction :=
+  instructions.forM processInstruction |>.run State.initial |>.snd.enabledMuls
+
+def solvePart2 (s : String) : Int := findInstructions s |> getEnabledMuls |>.sumBy (·.value)
 
 #guard solvePart2 exampleInputPart2 == 48
