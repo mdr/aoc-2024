c4953da0b094d3d5603798975c8d57ebb10f4931
Matt Russell <MattRussellUK@gmail.com>
2024-12-04 18:11:24 +0000

    update template

--- DayXX/Main.lean
+++ DayXX/Main.lean
@@ -7,9 +7,13 @@ def main : IO Unit := do
   IO.println "Part 1"
   let exampleInput <- IO.FS.readFile "Aoc2024/DayXX/inputs/example.txt"
   let puzzleInput <- IO.FS.readFile "Aoc2024/DayXX/inputs/input.txt"
-  IO.println s!"Example: {parseAndSolvePart1 exampleInput}"
-  IO.println s!"Puzzle: {parseAndSolvePart1 puzzleInput}" --
+  IO.println s!"Example: {solvePart1 exampleInput}"
+  let answerPart1 := solvePart1 puzzleInput
+  IO.println s!"Puzzle: {answerPart1}"
+  assert! (answerPart1 == -1)
   IO.println ""
   IO.println "Part 2"
-  IO.println s!"Example: {parseAndSolvePart2 exampleInput}"
-  IO.println s!"Puzzle: {parseAndSolvePart2 puzzleInput}" --
+  IO.println s!"Example: {solvePart2 exampleInput}"
+  let answerPart2 := solvePart2 puzzleInput
+  IO.println s!"Puzzle: {answerPart2}"
+  assert! (answerPart2 == -1)
