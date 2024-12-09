9033825e8ae1dcef124f529dc6dda5688cad82c2
Matt Russell <MattRussellUK@gmail.com>
2024-12-09 23:02:11 +0000

    day10 template

---
+++ Aoc2024/Day10/Solve.lean
@@ -0,0 +1,20 @@
+import Aoc2024.Utils
+import Aoc2024.Day10.Examples
+import Aoc2024.Day10.Parser
+import Aoc2024.Day10.Types
+
+-- Part 1
+
+private def solvePart1 (input : List Int) : Int := sorry
+
+def parseAndSolvePart1 (s : String): Except String Int := parseInput s |>.map solvePart1
+
+-- #guard parseAndSolvePart1 exampleInput == Except.ok -1
+
+-- Part 2
+
+private def solvePart2 (input : List Int) : Int := sorry
+
+def parseAndSolvePart2 (s : String): Except String Int := parseInput s |>.map solvePart2
+
+-- #guard parseAndSolvePart2 exampleInput == Except.ok -1
